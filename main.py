18b6613be47deff5a7b9a2eea0906c461abf524d
Sharath <sharathwelcomesyouin@gmail.com>
2022-06-11 16:32:44 +0530

    add slicing exercise

---
+++ main.py
@@ -0,0 +1,9 @@
+list_a = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+print(list_a[2:])
+print(list_a[:5])
+print(list_a[3:7])
+print(list_a[3:7:1])
+print(list_a[3:7:2])  # skip second item
+print(list_a[::2])  # include first item then skip second one
+print(list_a[::-1])  # reverse list
+# same is works for tuple
